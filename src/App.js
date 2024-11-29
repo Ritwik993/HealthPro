81e1c592cddf249c994c2d2e31d5481d379cbb40
Ritwik993 <ritwik2025@gmail.com>
2024-11-29 14:54:16 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-import { useState } from 'react';
-import './App.css';
-import Calculator from './components/Calculator/Calculator.jsx';
-import Navbar from './components/Navbar/Navbar';
-import Result from './components/Result/Result.jsx';
-import { createBrowserRouter, Outlet, Router, RouterProvider } from 'react-router-dom';
+import { useState } from "react";
+import "./App.css";
+import Calculator from "./components/Calculator/Calculator.jsx";
+import Navbar from "./components/Navbar/Navbar";
+import Result from "./components/Result/Result.jsx";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
 function App() {
-  const[percentage,setPercentage]=useState(0);
-  
-const AppLayout = () => {
-  return (
-    <>
-      <Navbar/>
-      <Outlet/>
-    </>
-  );
-};
-  const appRouter=createBrowserRouter([
-   {
-    path:'/',
-    element:<AppLayout/>,
-    children:[
-      {
-        path:'/',
-        element:<Calculator setPercentage={setPercentage}/>
-      },
-      {
-        path:'/result',
-        element:<Result percentage={percentage}/>
-      }
-    ]
-   }
-  ])
+  const [percentage, setPercentage] = useState(0);
+
+  const AppLayout = () => {
+    return (
+      <div className="">
+        <Navbar />
+        <Outlet />
+      </div>
+    );
+  };
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Calculator setPercentage={setPercentage} />,
+        },
+        {
+          path: "/result",
+          element: <Result percentage={percentage} />,
+        },
+      ],
+    },
+  ]);
   return (
     <div className="App">
-      <RouterProvider router={appRouter}/>
+      <RouterProvider router={appRouter} />
     </div>
   );
 }
